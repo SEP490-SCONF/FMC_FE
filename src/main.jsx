f920cbfd79b55fb8b1a1ba9d9715efed49dafa06
Anh.D.Nguyen <ffffffjj7@gmail.com>
2025-06-05 08:51:46 +0700

    FE-00: Modify - Update Template

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'animate.css/animate.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'jquery-ui-dist/jquery-ui.min.css'
+import 'magnific-popup/dist/magnific-popup.css'
+import 'odometer/themes/odometer-theme-default.css'
+import 'select2/dist/css/select2.min.css'
+import 'splitting/dist/splitting.css'
+import 'swiper/css';
+
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
