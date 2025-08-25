2cea432fcf53993974d17df80c3f1c14a758bf2a
Nam <156413717+kaizy2@users.noreply.github.com>
2025-08-25 23:58:45 +0700

    FE-MultiLanguage

--- src/services/TranslateService.jsx
+++ src/services/TranslateService.jsx
@@ -3,16 +3,13 @@ import { apiService } from "./ApiService";
 const API_URL = "/Translation";
 
 export const translateHighlightedText = async (text, targetLang = "en-US") => {
-  // chuẩn bị body JSON
-  const body = {
-    Text: text,
-    TargetLanguage: targetLang,
-  };
+  const formData = new FormData();
+  formData.append("Text", text);
+  formData.append("TargetLanguage", targetLang);
 
-  const res = await apiService.post(`${API_URL}/translate-highlight`, body, {
-    headers: { "Content-Type": "application/json" },
+  const res = await apiService.post(`${API_URL}/translate-highlight`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
   });
 
-  console.log("API response:", res);
-  return res?.translatedText || "";
+return res?.translatedText || "";
 };
